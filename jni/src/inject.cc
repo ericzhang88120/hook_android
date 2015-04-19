6fda7b868167de9ef7da3377b750bf0bf964202e
Eric zhang <ericzhang88120@gmail.com>
2015-04-20 03:43:48 +0800

    update inject file

--- jni/src/inject.cc
+++ jni/src/inject.cc
@@ -1,5 +1,6 @@
 #include "inject.h"
 
+const char *libc_path = "/system/lib/libc.so";
 int ptrace_attach(pid_t pid )
 {
 	int ret = -1;
@@ -13,12 +14,30 @@ int ptrace_attach(pid_t pid )
 	int status = 0;
 	waitpid(pid,&status,WUNTRACED);
 
+	if(WIFSTOPPED(status))
+	{
+		printf("child stopped,signal num = %d\n",WSTOPSIG(status));
+	}
+
+	return 0;
+}
+
+int ptrace_getregs(pid_t pid,struct pt_regs *regs)
+{
+	if(ptrace(PTRACE_GETREGS,pid,NULL,regs)<0)
+	{
+		perror("ptrace_getregs :Can not get register values");
+		return -1;
+	}
 	return 0;
 }
 
 int inject_remote_process(pid_t target_pid,const char * library_path,const char * function_name,const char * param,size_t param_size)
 {
 	int ret = -1;
+
+	struct pt_regs regs,original_regs;
+	void *mmap_addr, *dlopen_addr, *dlsym_addr, *dlclose_addr, *dlerror_addr; 
 
 	printf("Debug: start to inject process %d\n",target_pid);
 
@@ -28,5 +47,15 @@ int inject_remote_process(pid_t target_pid,const char * library_path,const char 
 		exit(0);
 	}
 
+	if(-1 == ptrace_getregs(target_pid,&regs))
+	{
+		printf("Debug: get process regs fail\n");
+		exit(0);
+	}
+
+	//save orignal registers
+	memcpy(&original_regs,&regs,sizeof(regs));
+
+	mmap_addr = get_remote_addr(target_pid, libc_path, (void *)mmap); 
 	
 }
--- jni/src/inject.h
+++ jni/src/inject.h
@@ -4,6 +4,8 @@
 
 extern "C"{
 	int inject_remote_process(pid_t target_pid,const char* library_path,const char* function_name,const char* param,size_t param_size);
-	
+
+	int ptrace_attach(pid_t pid );
+	int ptrace_getregs(pid_t pid,struct pt_regs *regs);
 }
 #endif
