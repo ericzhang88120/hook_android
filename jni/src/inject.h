c840356f96ef92624aa8a9634b77bfa554654917
Eric zhang <ericzhang88120@gmail.com>
2015-04-20 22:50:40 +0800

    add

--- jni/src/inject.h
+++ jni/src/inject.h
@@ -7,5 +7,8 @@ extern "C"{
 
 	int ptrace_attach(pid_t pid );
 	int ptrace_getregs(pid_t pid,struct pt_regs *regs);
+	void* get_remote_addr(pid_t target_pid,const char* module_name,void* local_addr);
+	void* get_module_base(pid_t pid,const char* module_name);
+	int ptrace_call_wrapper(pid_t target_pid,const char* func_name,void *func_addr,long *parameters,int param_num,struct pt_regs * regs);
 }
 #endif
